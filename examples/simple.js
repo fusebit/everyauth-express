e686b909ede9cf4384d96e6106c667c45b0db8cd
Tomasz Janczuk <tomasz@janczuk.org>
2022-04-13 08:50:22 -1000

    Sample update

--- examples/simple.js
+++ examples/simple.js
@@ -7,23 +7,27 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.redirect('/benn/slack');
+  res.redirect('/slack');
 });
 
 app.use(
-  '/:userId/slack',
-  everyauth.authorize('slack', { finishedUrl: '/finished', mapToUserId: (req) => req.params.userId })
+  '/slack',
+  everyauth.authorize('slack', {
+    finishedUrl: '/finished',
+    mapToUserId: (req) => 'user-123', // req.user.id in production
+  })
 );
 
 app.get('/finished', async (req, res) => {
-  const userId = req.query.userId;
+  const userId = 'user-123'; // req.user.id in production
 
   // Send a message over slack.
   const userCredentials = await everyauth.getIdentity('slack', userId);
   const slack = new WebClient(userCredentials?.accessToken);
+  const directMessageChannel = userCredentials?.native.authed_user.id;
   await slack.chat.postMessage({
     text: `Hello World from EveryAuth to ${userId}`,
-    channel: '#demo',
+    channel: directMessageChannel,
   });
 
   res.send(`Thanks for registering ${userId}`);
