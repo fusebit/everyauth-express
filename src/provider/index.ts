e9375c3b33d5c47774056ab30112293cb81a065d
Tomasz Janczuk <tomasz@janczuk.org>
2022-04-12 15:39:38 -1000

    Normalize credentials for all providers

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,5 +1,7 @@
 import * as slack from './slack';
+import * as oauth from './oauth';
 
 export * as slack from './slack';
+export * as oauth from './oauth';
 
 export type INative = slack.INative;
---
+++ src/provider/oauth.ts
@@ -0,0 +1,25 @@
+/**
+ * The results of an OAuth operation with Slack.
+ *
+ * {@link https://api.slack.com/methods/oauth.v2.access#examples | See the official Slack documentation for
+ * more details. }
+ */
+export interface INative {
+  ok: boolean;
+  team: { id: string; name: string };
+  scope: string;
+  app_id: string;
+  status: 'authenticated';
+  timestamp: number;
+  enterprise: null | { id: string; name: string };
+  token_type: 'bot' | 'user';
+  authed_user: { id: string; scope: string; access_token: string; token_type: string };
+  bot_user_id: string;
+  access_token: string;
+  is_enterprise_install: boolean;
+}
+
+export const normalize = (native: INative) => ({
+  native,
+  accessToken: native.access_token,
+});
