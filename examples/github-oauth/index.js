cf21861930080963457b1598e0f183ae1fca9ba6
Ruben Restrepo <4001529+degrammer@users.noreply.github.com>
2022-04-22 12:16:02 -0500

    Add GitHub OAuth App example (#8)
    * Add GitHub OAuth App example

---
+++ examples/github-oauth/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const everyauth = require('@fusebit/everyauth-express');
+const { Octokit } = require('octokit');
+const { v4: uuidv4 } = require('uuid');
+const cookieSession = require('cookie-session');
+
+const app = express();
+const port = 3000;
+
+// Get userId from the authorization redirect or via session if already authorized.
+const handleSession = (req, res, next) => {
+  if (req.query.userId) {
+    req.session.userId = req.query.userId;
+  }
+  if (!req.session.userId) {
+    return res.redirect('/');
+  }
+  return next();
+};
+
+app.use(
+  cookieSession({
+    name: 'session',
+    secret: 'secret',
+  })
+);
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect(`/authorize/${uuidv4()}`);
+  }
+  res.redirect('/finished');
+});
+
+app.use(
+  '/authorize/:userId',
+  (req, res, next) => {
+    if (!req.params.userId) {
+      return res.redirect('/');
+    }
+    return next();
+  },
+  everyauth.authorize('githuboauth', {
+    // The endpoint of your app where control will be returned afterwards
+    finishedUrl: '/finished',
+    // The user ID of the authenticated user the credentials will be associated with
+    mapToUserId: (req) => req.params.userId,
+  })
+);
+
+app.get('/finished', handleSession, async (req, res) => {
+  const userCredentials = await everyauth.getIdentity('githuboauth', req.session.userId);
+  const client = new Octokit({ auth: userCredentials?.accessToken });
+  const { data } = await client.rest.users.getAuthenticated();
+  const { data: repos } = await client.request('GET /user/repos', {});
+  res.render('index', {
+    title: `GitHub Profile for ${data.login}`,
+    ...data,
+    used_storage: Math.round((data.disk_usage * 100) / data.plan.space, 2),
+    public_repos: repos,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
