45482bc0a9336a3e392b7a8e9f2677064cf9eaf5
Benn Bollay <bb.github@magitech.org>
2022-04-29 19:53:48 -0700

    Fix path-based profile search termination condition on Windows

--- test/profile.recurse.test.ts
+++ test/profile.recurse.test.ts
@@ -4,6 +4,4 @@ test('Load a profile by walking upwards, looking for a .fusebit directory', asyn
   const profile = await everyauth.profile.getAuthedProfile();
 
   expect(profile.accessToken?.length).toBeGreaterThan(1);
-
-  expect(1).toBe(1);
 });
