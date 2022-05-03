5716503c3f31d526daa8a0ccb538ab464605f72a
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-05-03 14:41:40 -0400

    Prettier Fixes

--- examples/asana-googlecalendar/index.js
+++ examples/asana-googlecalendar/index.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const moment = require("moment");
-const { v4: uuidv4 } = require("uuid");
-const cookieSession = require("cookie-session");
-const mustacheExpress = require("mustache-express");
+const express = require('express');
+const moment = require('moment');
+const { v4: uuidv4 } = require('uuid');
+const cookieSession = require('cookie-session');
+const mustacheExpress = require('mustache-express');
 
-const asana = require("asana");
-const { google } = require("googleapis");
-const everyauth = require("@fusebit/everyauth-express");
+const asana = require('asana');
+const { google } = require('googleapis');
+const everyauth = require('@fusebit/everyauth-express');
 
 const app = express();
 const port = 3000;
 const SERVICES = ['asana', 'google'];
 
-app.engine("mustache", mustacheExpress());
-app.set("view engine", "mustache");
-app.set("views", __dirname + "/views");
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cookieSession({ name: "session", secret: "secret" }));
+app.use(cookieSession({ name: 'session', secret: 'secret' }));
 
 // Initialize auth route for every service
 SERVICES.forEach((service) => {
-
   app.use(
     `/${service}/authorize/:userId`,
     everyauth.authorize(service, {
@@ -29,7 +28,6 @@ SERVICES.forEach((service) => {
       mapToUserId: (req) => req.params.userId,
     })
   );
-
 });
 
 // Check if all sessions are completed
@@ -44,11 +42,10 @@ const isAllSessionsComplete = (req, res, next) => {
   });
   res.locals.fullyAuthenticated = isSessionComplete;
   return next();
-}
+};
 
 // Set Session UserId for service
 const setSession = (req, res, next) => {
-
   // If it returns a userID, retreive Service Name
   if (req.query.userId) {
     // Service Name will only be returned after an auth has been completed
@@ -63,7 +60,6 @@ const setSession = (req, res, next) => {
 
 // Check is all sessions are complete
 const ensureSession = (req, res, next) => {
-
   SERVICES.forEach((service) => {
     // For the missing service, redirect to authorization flow for that service
     if (!req.session[`${service}UserId`]) {
@@ -73,24 +69,20 @@ const ensureSession = (req, res, next) => {
   return next();
 };
 
-app.get("/", isAllSessionsComplete, (req, res) => {
-
+app.get('/', isAllSessionsComplete, (req, res) => {
   if (res.locals.fullyAuthenticated) {
     return res.redirect(`/tasks`);
   }
-  return res.render("index", { userId: uuidv4() });
+  return res.render('index', { userId: uuidv4() });
 });
 
-
-app.get("/tasks", setSession, ensureSession, async (req, res) => {
+app.get('/tasks', setSession, ensureSession, async (req, res) => {
   // Retrieve an always fresh access token using userId
-  const asanaCredentials = await everyauth.getIdentity("asana", req.session.asanaUserId);
-  const googleCredentials = await everyauth.getIdentity("google", req.session.googleUserId);
+  const asanaCredentials = await everyauth.getIdentity('asana', req.session.asanaUserId);
+  const googleCredentials = await everyauth.getIdentity('google', req.session.googleUserId);
 
   // Call Asana API
-  const asanaClient = asana.Client.create().useAccessToken(
-    asanaCredentials.accessToken
-  );
+  const asanaClient = asana.Client.create().useAccessToken(asanaCredentials.accessToken);
 
   const me = await asanaClient.users.me();
   const workspace = me.workspaces[0].gid;
@@ -117,19 +109,19 @@ app.get("/tasks", setSession, ensureSession, async (req, res) => {
   myAuth.setCredentials({ access_token: googleCredentials.accessToken });
   google.options({ auth: myAuth });
 
-  const calendar = google.calendar({ version: "v3" });
+  const calendar = google.calendar({ version: 'v3' });
   const calendarEvents = await calendar.events.list({
     auth: myAuth,
-    calendarId: "primary",
+    calendarId: 'primary',
     timeMin: new Date(),
   });
 
   const calendarEventsList = calendarEvents.data.items.map((calendarItem) => {
     // Google Calendar API Returns Inconsistent Start Times
     // So this is a workaround to handle the data and normalize it
-    var startTimeFromNow = "";
+    var startTimeFromNow = '';
     if (!calendarItem.start) {
-      startTimeFromNow = "";
+      startTimeFromNow = '';
     } else if (calendarItem.start.dateTime) {
       startTimeFromNow = moment(calendarItem.start.dateTime).fromNow();
     } else if (calendarItem.start.date) {
@@ -142,10 +134,8 @@ app.get("/tasks", setSession, ensureSession, async (req, res) => {
     };
   });
 
-  res.render("tasklist", { TaskListData: { taskDetails, calendarEventsList } });
-
+  res.render('tasklist', { TaskListData: { taskDetails, calendarEventsList } });
 });
-
 
 // Add a new event to a calendar by ID
 app.post('/tasks/calendar/', setSession, ensureSession, async (req, res) => {
