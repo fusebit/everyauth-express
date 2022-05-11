6b9d158701c293468a685cd5671df7e5512f2c6b
degrammer <ruben@fusebit.io>
2022-05-11 14:54:51 -0500

    PagerDuty example

--- examples/pagerduty/index.js
+++ examples/pagerduty/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
-const handleSession = (req, res, next) => {
+const ensureSession = (req, res, next) => {
   if (req.query.userId) {
     req.session.userId = req.query.userId;
   }
@@ -33,32 +33,71 @@ app.get('/', (req, res) => {
 
 app.use(
   '/authorize/:userId',
-  (req, res, next) => {
-    if (!req.params.userId) {
-      return res.redirect('/');
-    }
-
-    return next();
-  },
   everyauth.authorize('pagerduty', {
     finishedUrl: '/finished',
     mapToUserId: (req) => req.params.userId,
   })
 );
 
-app.get('/finished', handleSession, async (req, res) => {
+app.get('/finished', ensureSession, async (req, res) => {
+  const { error } = req.query;
+  if (error) {
+    return res.render('index', {
+      error,
+    });
+  }
+
   const creds = await everyauth.getIdentity('pagerduty', req.session.userId);
   const sdk = api({ token: creds.accessToken, tokenType: 'bearer' });
-  const services = await (await sdk.get('/services')).resource;
-  const me = await (await sdk.get('/users/me')).data.user;
+  const services = (await sdk.get('/services')).resource;
+  const me = (await sdk.get('/users/me')).data.user;
   res.render('index', {
     title: 'Welcome to EveryAuth PagerDuty Demo App!',
     name: me.name,
     avatar_url: me.avatar_url,
-    svcs: services,
+    services,
+  });
+});
+
+app.post('/incident', ensureSession, async (req, res) => {
+  const { error, service } = req.query;
+  if (error) {
+    return res.render('index', {
+      error,
+    });
+  }
+
+  if (!service) {
+    return res.render('index', {
+      error: 'Missing a service to trigger the incident',
+    });
+  }
+
+  const creds = await everyauth.getIdentity('pagerduty', req.session.userId);
+  const sdk = api({ token: creds.accessToken, tokenType: 'bearer' });
+  const me = (await sdk.get('/users/me')).data.user;
+  
+  const incident = await sdk.post('/incidents', {
+    data: {
+      'incident': {
+        'type': 'incident',
+        'title': 'The server is on fire.',
+        'service': {
+          'id': req.query.service,
+          'type': 'service_reference'
+        }
+      }
+    }
+  });
+
+  res.render('incident', {
+    title: 'Incident created',
+    name: me.name,
+    avatar_url: me.avatar_url,
+    incident: incident.data.incident,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app running on http://localhost:${PORT}`);
 });
